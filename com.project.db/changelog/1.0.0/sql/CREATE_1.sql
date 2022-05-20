b8ff59301f6c3b77b908d16af888bcbc6a6987a2
mauricio.echeverria <ext_mecheverriav@falabella.cl>
2022-05-20 18:11:34 -0400

    com.project.db

---
+++ com.project.db/changelog/1.0.0/sql/CREATE_1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE
+  audit_events (id STRING, created_at TIMESTAMP, event_id STRING, event_type STRING, event_date TIMESTAMP, json_event STRING, message STRING)
+PARTITION BY
+  TIMESTAMP_TRUNC(event_date, MONTH)
+OPTIONS(
+  partition_expiration_days=280,
+  require_partition_filter=true
+)
